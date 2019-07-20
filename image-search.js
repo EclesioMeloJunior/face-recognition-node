05520320d0bb8fe2384abd950b0a3ec7696b1621
Eclesio Ferreira <eclesio@alphasoftware.br>
2019-07-20 11:59:32 -0400

    feat: image search done

---
+++ image-search.js
@@ -0,0 +1,30 @@
+const google = require("googleapis").google;
+const customSearch = google.customsearch("v1");
+
+const googleSearchCredentials = require("./.credentials/google-credentials.json");
+
+async function imageSearch(content) {
+	content.sentence = buildSentenceToSearch(content);
+	content.imagesLinks = await SearchBySentenceAndReturnLinks(content.sentence);
+
+	function buildSentenceToSearch(content) {
+		return `${content.searchTerm} ${content.keywords.join(" ")}`;
+	}
+
+	async function SearchBySentenceAndReturnLinks(sentence) {
+		const response = await customSearch.cse.list({
+			auth: googleSearchCredentials.apiKey,
+			cx: googleSearchCredentials.searchEngineID,
+			q: sentence,
+			searchType: "image",
+			num: 3
+		});
+
+		console.log(response);
+
+		const imagesUrl = response.data.items.map(item => item.link);
+		return imagesUrl;
+	}
+}
+
+module.exports = imageSearch;
--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-function start() {
+var readlineSync = require("readline-sync");
+const components = {
+	imageSearch: require("./image-search")
+};
+
+async function start() {
 	const content = {};
+
+	content.searchTerm = askSearchTerm();
+	content.keywords = askKeywordsAndParseToArray();
+
+	await components.imageSearch(content);
+
+	console.log(content);
+
+	function askSearchTerm() {
+		return readlineSync.question("Type your search: ");
+	}
+
+	function askKeywordsAndParseToArray() {
+		const keywordsSeparatedByComma = askKeywords();
+		const keywordArray = fromCSKeywordsToArray(keywordsSeparatedByComma);
+
+		return keywordArray;
+
+		function fromCSKeywordsToArray(keywords) {
+			const keywordArray = [];
+
+			for (const keyword of keywords.split(",")) {
+				keywordArray.push(keyword.trim());
+			}
+
+			return keywordArray;
+		}
+	}
+
+	function askKeywords() {
+		return readlineSync.question("Type some keywords separated by comma: ");
+	}
 }
 
 start();
